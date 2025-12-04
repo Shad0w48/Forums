9587154a495aed62abdd76b2ce06aa27bedc0f40
Student-2024 <student-2024@ashalim.org>
2025-12-04 09:05:03 +0200

    sharedpref

---
+++ app/src/main/java/com/ilya/forums/RegisterActivity.java
@@ -0,0 +1,153 @@
+package com.ilya.forums;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.ilya.forums.model.User;
+import com.ilya.forums.services.DatabaseService;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "Register";
+    EditText etFname, etLname, etMail, etPhone, etPassword;
+    String fName, lName, email, phone, password;
+    Button btnSubmit;
+    private DatabaseService databaseService;
+    private FirebaseAuth mAuth;
+    public static final String MyPreferences="MyPrefs";
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        sharedPreferences=getSharedPreferences(MyPreferences, Context.MODE_PRIVATE);
+
+        databaseService=DatabaseService.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        etFname = findViewById(R.id.etFNameRegister);
+        etLname = findViewById(R.id.etLNameRegister);
+        etMail = findViewById(R.id.etEmailRegister);
+        etPhone = findViewById(R.id.etPhoneRegister);
+        etPassword = findViewById(R.id.etPasswordRegister);
+        btnSubmit = findViewById(R.id.btnRegister);
+        btnSubmit.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == btnSubmit.getId()) {
+            Log.d(TAG, "onClick: Register button clicked");
+
+            /// get the input from the user
+            fName = etFname.getText().toString();
+            lName = etLname.getText().toString();
+            email = etMail.getText().toString();
+            phone = etPhone.getText().toString();
+            password = etPassword.getText().toString();
+
+
+            /// Validate input
+            Log.d(TAG, "onClick: Registering user...");
+
+            /// Register user
+            registerUser(fName, lName, phone, email, password);
+        }
+    }
+
+    /// Register the user
+    private void registerUser(String fname, String lname, String phone, String email, String password) {
+        Log.d(TAG, "registerUser: Registering user...");
+
+        databaseService.checkIfEmailExists(email, new DatabaseService.DatabaseCallback<>() {
+            @Override
+            public void onCompleted(Boolean exists) {
+                if (exists) {
+                    Log.e(TAG, "onCompleted: Email already exists");
+                    /// show error message to user
+                    Toast.makeText(RegisterActivity.this, "Email already exists", Toast.LENGTH_SHORT).show();
+                } else {
+                    mAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(authTask -> {
+
+                                if (!authTask.isSuccessful()) {
+                                    Toast.makeText(RegisterActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+
+                                String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+                                User user = new User(uid, fname, lname, email, phone, password, null);
+
+                                createUserInDatabase(user);
+
+                            });
+                }
+            }
+            @Override
+            public void onFailed(Exception e) {
+                Log.e(TAG, "onFailed: Failed to check if email exists", e);
+                /// show error message to user
+                Toast.makeText(RegisterActivity.this, "Failed to register user", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void createUserInDatabase(User user) {
+        databaseService.createNewUser(user, new DatabaseService.DatabaseCallback<Void>() {
+            @Override
+            public void onCompleted(Void object) {
+                Log.d(TAG, "createUserInDatabase: User created successfully");
+                /// save the user to shared preferences
+
+                Log.d(TAG, "createUserInDatabase: Redirecting to MainActivity");
+                /// Redirect to MainActivity and clear back stack to prevent user from going back to register screen
+
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                editor.putString("email", email);
+                editor.putString("password", password);
+
+                editor.commit();
+
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                /// clear the back stack (clear history) and start the MainActivity
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onFailed(Exception e) {
+                Log.e(TAG, "createUserInDatabase: Failed to create user", e);
+                /// show error message to user
+                Toast.makeText(RegisterActivity.this, "Failed to register user", Toast.LENGTH_SHORT).show();
+                /// sign out the user if failed to register
+
+            }
+        });
+    }
+
+
+}
